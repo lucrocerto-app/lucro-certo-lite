edbdbd14043bd061492b1756dd5da81e12140b64
lucrocerto-app <lucrocerto.app@proton.me>
2026-02-02 13:07:02 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'lucro-certo-lite-v1';
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => response || fetch(e.request))
+    );
+
+});
+
+
